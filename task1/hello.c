93d369e9d29a7e430fa7f015ce4d64e094b11fa4
joddiy <joddiy@qq.com>
2019-03-25 15:12:38 +0800

    modify hello.c

--- task1/hello.c
+++ task1/hello.c
@@ -2,12 +2,16 @@
 #include <linux/init.h>
 #include <linux/module.h>
 MODULE_LICENSE("GPL");
-static int hello_init(void)
+
+static char *whom = "world";
+module_param(whom, charp, S_IRUGO);
+
+static int __init hello_init(void)
 {
-    printk(KERN_ALERT "Hello, world\n");
+    printk(KERN_ALERT "Hello, %s\n", whom);
     return 0;
 }
-static void hello_exit(void)
+static void __exit hello_exit(void)
 {
     printk(KERN_ALERT "Goodbye, cruel world\n");
 }
