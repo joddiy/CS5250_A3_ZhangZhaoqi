fa3f5ccfbcbebbc39f7f1d9e1a560762c85a4d83
joddiy <joddiy@qq.com>
2019-03-25 15:14:08 +0800

    modify hello.c

--- task1/hello.c
+++ task1/hello.c
@@ -6,12 +6,12 @@ MODULE_LICENSE("GPL");
 static char *whom = "world";
 module_param(whom, charp, S_IRUGO);
 
-static int __init hello_init(void)
+static int hello_init(void)
 {
     printk(KERN_ALERT "Hello, %s\n", whom);
     return 0;
 }
-static void __exit hello_exit(void)
+static void hello_exit(void)
 {
     printk(KERN_ALERT "Goodbye, cruel world\n");
 }
